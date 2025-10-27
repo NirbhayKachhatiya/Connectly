6334fffb647f2206c1bfa20a6672c3f76d486333
NirbhayKachhatiya <nirbhaykck@gmail.com>
2025-10-27 10:21:11 +0530

    Initilaising frontend and backend

---
+++ backend/src/server.js
@@ -0,0 +1,8 @@
+import express from "express";
+import { ENV } from "./config/env.js";
+
+const app  = express();
+
+app.get("/",(req,res)=>{res.send("NO")});
+
+app.listen(ENV.PORT,()=>console.log("Server started on ",ENV.PORT))
